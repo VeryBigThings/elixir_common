90b16b4527f21c34ff9ae3264ee5457febd9f043
Saša Jurić <sasa.juric@gmail.com>
2019-09-12 15:44:31 +0200

    Add `eventually` test helper

--- lib/vbt/test_helper.ex
+++ lib/vbt/test_helper.ex
@@ -1,6 +1,10 @@
 defmodule VBT.TestHelper do
   @moduledoc "Various helpers which can be useful in tests."
 
+  # ------------------------------------------------------------------------
+  # API
+  # ------------------------------------------------------------------------
+
   @doc """
   Returns a unique positive integer.
 
@@ -17,4 +21,49 @@ defmodule VBT.TestHelper do
   """
   @spec unique_positive_integer() :: pos_integer
   def unique_positive_integer, do: :erlang.unique_integer([:positive, :monotonic])
+
+  @doc """
+  Verifies that the provided assertion is eventually met.
+
+  This function is useful when you want to assert a condition that doesn't necessarily hold
+  now, but should be met in a near future. Suppose that we want to verify that a token expires
+  after one second. We can do it as:
+
+      iex> VBT.TestHelper.eventually(fn -> assert token_expired?() end, attempts: 100, delay: 100)
+      true
+
+  In the call above, we're instructing `eventually` to repeatedly invoke the assertion, sleeping
+  100ms between two consecutive attempts. The function returns when the assertion succeeds. If the
+  assertion didn't succeed in the given number of attempts, the test will fail.
+
+  Notice that the maximum number of attempts is set to 100. This means that we're waiting for at
+  most 10 seconds. Since the timing logic is never completely precise, it's possible that the
+  token expires a bit later. If the test machine is very busy, the timing mismatch might even
+  be much larger. By choosing a significantly larger maximum waiting time, we're reducing the
+  chance of a test randomly failing on a busy CI server. At the same time, the delay is reasonably
+  small, so the test will succeed at most 100ms after the token expires. In most cases, the
+  test will take about 1 second.
+
+  Options:
+
+    - `:attempts` - The number of attempts before giving up. The default value is 10.
+    - `:delay` - Sleep time in ms between two consecutive attempts. The default value is 100.
+  """
+  @spec eventually((() -> res), attempts: pos_integer, delay: non_neg_integer) :: res
+        when res: var
+  def eventually(fun, opts \\ []),
+    do: eventually(fun, Keyword.get(opts, :attempts, 10), Keyword.get(opts, :delay, 100))
+
+  # ------------------------------------------------------------------------
+  # Private
+  # ------------------------------------------------------------------------
+
+  defp eventually(fun, attempts, delay) do
+    fun.()
+  rescue
+    e in [ExUnit.AssertionError] ->
+      if attempts == 1, do: reraise(e, __STACKTRACE__)
+      Process.sleep(delay)
+      eventually(fun, attempts - 1, delay)
+  end
 end
--- test/vbt/test_helper_test.exs
+++ test/vbt/test_helper_test.exs
@@ -18,4 +18,32 @@ defmodule VBT.TestHelperTest do
       |> Enum.each(fn [previous, next] -> assert previous < next end)
     end
   end
+
+  describe "eventually" do
+    test "raises an assertion error if the condition is not met after max attempts" do
+      e =
+        assert_raise(
+          ExUnit.AssertionError,
+          fn ->
+            VBT.TestHelper.eventually(
+              fn ->
+                current_value = Process.get(:expected_value, 0)
+                Process.put(:expected_value, current_value + 1)
+                assert current_value == 5
+              end,
+              attempts: 1,
+              delay: 10
+            )
+          end
+        )
+
+      assert e.message == "Assertion with == failed"
+    end
+
+    defp token_expired?() do
+      current_value = Process.get(:expected_value, 0)
+      Process.put(:expected_value, current_value + 1)
+      current_value == 5
+    end
+  end
 end
