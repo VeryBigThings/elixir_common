14ff5c920b825cc975b00f092668d3d599c15a4c
Saša Jurić <sasa.juric@gmail.com>
2019-10-25 16:30:27 +0200

    Print manual instructions

--- lib/mix/tasks/vbt/bootstrap.ex
+++ lib/mix/tasks/vbt/bootstrap.ex
@@ -11,5 +11,47 @@ defmodule Mix.Tasks.Vbt.Bootstrap do
       ~w/makefile docker circleci heroku github_pr_template credo dialyzer/,
       &Mix.Task.run("vbt.gen.#{&1}", args)
     )
+
+    Mix.shell().info(manual_instructions())
+  end
+
+  defp manual_instructions do
+    """
+    You also need to make the following changes in your mix.exs file:
+
+        def project do
+          [
+            # ...
+            preferred_cli_env: preferred_cli_env(),
+            dialyzer: dialyzer()
+          ]
+        end
+
+        def deps do
+          [
+            # ...
+            {:dialyxir, "~> 0.5", runtime: false}
+          ]
+        end
+
+        defp aliases do
+          [
+            # ...
+            test: ["ecto.create --quiet", "ecto.migrate", "test"],
+            credo: ~w/compile credo/
+          ]
+        end
+
+        defp preferred_cli_env() do
+          [credo: :test, dialyzer: :test]
+        end
+
+        defp dialyzer() do
+          [
+            plt_add_apps: [:ex_unit],
+            ignore_warnings: "dialyzer.ignore-warnings"
+          ]
+        end
+    """
   end
 end
