0732aa33ed4e9e4c6943599f02e0500f2bf6cb34
Saša Jurić <sasa.juric@gmail.com>
2020-01-22 17:51:09 +0100

    Modify endpoint configuration

--- lib/mix/tasks/vbt/bootstrap.ex
+++ lib/mix/tasks/vbt/bootstrap.ex
@@ -24,6 +24,7 @@ defmodule Mix.Tasks.Vbt.Bootstrap do
     |> add_standard_deps()
     |> configure_preferred_cli_env()
     |> configure_dialyzer()
+    |> configure_endpoint()
     |> store_source_files!()
   end
 
@@ -76,14 +77,64 @@ defmodule Mix.Tasks.Vbt.Bootstrap do
     )
   end
 
+  defp configure_endpoint(source_files) do
+    source_files
+    |> remove_endpoint_configs()
+    |> update_in([:endpoint], &setup_runtime_endpoint_config/1)
+  end
+
+  defp setup_runtime_endpoint_config(endpoint_file) do
+    SourceFile.add_to_module(
+      endpoint_file,
+      """
+
+      @impl Phoenix.Endpoint
+      def init(_type, config) do
+        config =
+          config
+          |> Keyword.put(:secret_key_base, System.fetch_env!("SECRET_KEY_BASE"))
+          |> Keyword.update(:url, url_config(), &Keyword.merge(&1, url_config()))
+          |> Keyword.update(:http, http_config(), &(http_config() ++ (&1 || [])))
+
+        {:ok, config}
+      end
+
+      defp url_config, do: [host: System.fetch_env!("HOST")]
+      defp http_config, do: [:inet6, port: System.fetch_env!("PORT")]
+      """
+    )
+  end
+
+  defp remove_endpoint_configs(source_files) do
+    Enum.reduce(
+      ~w/config dev_config test_config prod_config/a,
+      source_files,
+      fn config, source_files ->
+        update_in(
+          source_files[config],
+          &ConfigFile.update_endpoint_config(
+            &1,
+            fn opts -> Keyword.drop(opts, ~w/url http secret_key_base/a) end
+          )
+        )
+      end
+    )
+  end
+
   defp source_files do
     %{
       mix: SourceFile.load!("mix.exs"),
       config: SourceFile.load!("config/config.exs"),
       dev_config: SourceFile.load!("config/dev.exs"),
       test_config: SourceFile.load!("config/test.exs"),
-      prod_config: SourceFile.load!("config/prod.exs")
+      prod_config: SourceFile.load!("config/prod.exs"),
+      endpoint: load_web_file("endpoint.ex")
     }
+  end
+
+  defp load_web_file(location) do
+    app = Keyword.fetch!(Mix.Project.config(), :app)
+    SourceFile.load!(Path.join(["lib", "#{app}_web", location]))
   end
 
   defp store_source_files!(source_files),
