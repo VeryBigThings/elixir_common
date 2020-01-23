c0ef63f807b0ac47d41e1257f70f2b1749d46d5f
Saša Jurić <sasa.juric@gmail.com>
2020-01-23 12:48:20 +0100

    Improve bootstrap test
    This test now works similarly to VCR. It copies the test project source into a tmp folder, runs vbt.bootstrap there, and compares the differences to the expected folder.

--- test/vbt/integration/skafolder_test.exs
+++ test/vbt/integration/skafolder_test.exs
@@ -2,53 +2,103 @@ defmodule VBT.Integration.SkafolderTest do
   use ExUnit.Case, async: true
   @moduletag :integration
 
-  setup_all do
+  @tag timeout: :timer.minutes(3)
+  test "vbt.bootstrap" do
+    initialize_project()
+
+    # hardcoding the generated secret key to ensure reproducible output
+    System.put_env("SECRET_KEY_BASE", "test_only_secret_key_base")
+    assert {_output, 0} = mix(~w/vbt.bootstrap --force/)
+
+    # make sure that the project can be compiled after the changes have been applied
+    assert {_output, 0} = mix(~w/compile/)
+
+    with {:error, differences} <- differences() do
+      if System.get_env("SYNC_BOOTSTRAP_TEST", "false") != "true" do
+        flunk(error_message(differences))
+      else
+        sync_differences(differences)
+
+        flunk("""
+        Differences have been synchronized.
+        Please rerun the test again without the SYNC_BOOTSTRAP_TEST env set.
+        """)
+      end
+    end
+  end
+
+  defp initialize_project do
+    File.mkdir_p!(build_path())
+    File.cp_r!(source_path(), build_path())
     mix!(~w/deps.get/)
     mix!(~w/compile/)
-    :ok
   end
 
-  setup do
-    Enum.each(expected_files(), &File.rm/1)
-    :ok
+  defp differences do
+    output_files = MapSet.new(source_files(build_path()))
+    expected_files = MapSet.new(source_files(expected_path()))
+
+    missing = Enum.sort(MapSet.difference(expected_files, output_files))
+    unexpected = Enum.sort(MapSet.difference(output_files, expected_files))
+
+    changed =
+      expected_files
+      |> MapSet.intersection(output_files)
+      |> Stream.filter(fn file ->
+        output = File.read!(Path.join(build_path(), file))
+        expected = File.read!(Path.join(expected_path(), file))
+        output != expected
+      end)
+      |> Enum.sort()
+
+    if Enum.all?([missing, unexpected, changed], &Enum.empty?/1),
+      do: :ok,
+      else: {:error, %{missing: missing, unexpected: unexpected, changed: changed}}
   end
 
-  test "vbt.bootstrap generates expected files" do
-    current_files = all_project_files()
-    assert {_output, 0} = mix(~w/vbt.bootstrap/)
-
-    new_files = MapSet.difference(all_project_files(), current_files)
-    assert new_files == expected_files()
-  end
-
-  defp all_project_files do
-    Path.join(project_path(), "**")
-    |> Path.wildcard(match_dot: true)
+  defp source_files(folder) do
+    Path.wildcard("#{folder}/**", match_dot: true)
+    |> Stream.reject(&String.starts_with?(&1, "#{folder}/_build"))
+    |> Stream.reject(&String.starts_with?(&1, "#{folder}/deps"))
     |> Stream.reject(&File.dir?/1)
-    |> MapSet.new()
+    |> Stream.map(&Path.relative_to(&1, folder))
+    |> Enum.sort()
   end
 
-  defp expected_files do
-    Enum.into(
-      ~w[
-        .dockerignore
-        .circleci/config.yml
-        .env.development
-        Dockerfile
-        Makefile
-        docker-compose.yml
-        entrypoint.sh
-        heroku.yml
-        rel/bin/migrate.sh
-        rel/bin/rollback.sh
-        rel/bin/seed.sh
-        .credo.exs
-        .github/pull_request_template.md
-        dialyzer.ignore-warnings
-      ],
-      MapSet.new(),
-      &Path.join(project_path(), &1)
-    )
+  defp error_message(differences) do
+    error_list =
+      Enum.concat([
+        Enum.map(differences.missing, &"  - #{&1} is missing"),
+        Enum.map(differences.unexpected, &"  - #{&1} is unexpected"),
+        Enum.map(differences.changed, &"  - #{&1} is changed")
+      ])
+      |> Enum.sort()
+      |> Enum.join("\n")
+
+    flunk("""
+    Bootstrapped project doesn't have the expected shape:
+
+    #{error_list}
+
+    To automatically sync the expected folder, rerun the test as:
+
+        SYNC_BOOTSTRAP_TEST=true mix test ...
+    """)
+  end
+
+  defp sync_differences(differences) do
+    Enum.each(differences.missing, &File.rm!(Path.join(expected_path(), &1)))
+
+    differences.unexpected
+    |> Stream.concat(differences.changed)
+    |> Enum.each(&copy_to_expected/1)
+  end
+
+  defp copy_to_expected(file) do
+    source = Path.join(build_path(), file)
+    destination = Path.join(expected_path(), file)
+    File.mkdir_p!(Path.dirname(destination))
+    File.copy!(source, destination)
   end
 
   defp mix!(args) do
@@ -56,7 +106,9 @@ defmodule VBT.Integration.SkafolderTest do
     output
   end
 
-  defp mix(args), do: System.cmd("mix", args, stderr_to_stdout: true, cd: project_path())
+  defp mix(args), do: System.cmd("mix", args, stderr_to_stdout: true, cd: build_path())
 
-  defp project_path, do: Path.join(~w/test_projects skafolder_tester/)
+  defp source_path, do: Path.join(~w/test_projects skafolder_tester/)
+  defp build_path, do: Path.join(~w/tmp skafolder_tester/)
+  defp expected_path, do: Path.join(~w/test_projects expected_state/)
 end
