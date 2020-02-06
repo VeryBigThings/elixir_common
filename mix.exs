aec48c7b35c4a5b034d4f721d9f6be6ac476861f
Saša Jurić <sasa.juric@gmail.com>
2020-02-06 15:19:23 +0100

    Update to oban 1.0

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule VBT.Credo.MixProject do
       {:ecto, "~> 3.0"},
       {:ex_aws, "~> 2.1"},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false},
-      {:oban, "~> 0.12"},
+      {:oban, "~> 1.0"},
       {:parent, "~> 0.8"},
       {:phoenix_html, "~> 2.0"},
       {:phoenix, "~> 1.4"},
