d58db7ebff75e3869ab1b0909549645acd17ea8c
Saša Jurić <sasa.juric@gmail.com>
2020-03-19 08:14:54 +0100

    Remove needless tmp files
    These were accidentally added in 3ae0f5e365d6bf971dab69dfd2e8331d4d187dfe

--- tmp/skafolder_tester/rel/bin/check_config.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-script_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
-args="$@"
-$script_dir/skafolder_tester eval "SkafolderTesterApp.Release.check_config()"
--- tmp/skafolder_tester/test/test_helper.exs
+++
@@ -1,3 +0,0 @@
-ExUnit.start()
-Ecto.Adapters.SQL.Sandbox.mode(SkafolderTester.Repo, :manual)
-Mox.defmock(VBT.TestAwsClient, for: ExAws.Behaviour)
