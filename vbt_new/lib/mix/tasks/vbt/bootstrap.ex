63f6e7c67232af41dda855d06d092d3bb82d15f8
Saša Jurić <sasa.juric@gmail.com>
2020-03-31 12:22:24 +0200

    Adapt config generation

--- vbt_new/lib/mix/tasks/vbt/bootstrap.ex
+++ vbt_new/lib/mix/tasks/vbt/bootstrap.ex
@@ -14,7 +14,7 @@ defmodule Mix.Tasks.Vbt.Bootstrap do
 
     Enum.each(
       ~w/makefile docker circleci release github_pr_template credo dialyzer formatter_config
-      tool_versions aws_mock config/,
+      tool_versions aws_mock/,
       &Mix.Task.run("vbt.gen.#{&1}", args)
     )
 
