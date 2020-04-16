7581cbd70f411f0ee70bee3e83552b8a28e30fc3
Saša Jurić <sasa.juric@gmail.com>
2020-04-16 14:54:57 +0200

    Move implementation to VBT.Repo

--- lib/vbt/repo.ex
+++ lib/vbt/repo.ex
@@ -3,6 +3,8 @@ defmodule VBT.Repo do
   Wrapper around `Ecto.Repo` with a few additional helper functions.
   """
 
+  import Ecto.Query
+
   @type trans_fun ::
           (() -> {:ok, any} | {:error, any})
           | (module -> {:ok, any} | {:error, any})
@@ -93,93 +95,113 @@ defmodule VBT.Repo do
       @behaviour VBT.Repo
 
       @impl VBT.Repo
-      def fetch(schema, id, opts \\ []) do
-        unless VBT.Repo.schema_module?(schema), do: raise(ArgumentError, "expected a schema")
-
-        case schema.__schema__(:primary_key) do
-          [primary_key] -> fetch_by(schema, [{primary_key, id}], opts)
-          _other -> raise(ArgumentError, "#{inspect(schema)} must have exactly one primary key")
-        end
-      end
+      def fetch(schema, id, opts \\ []), do: VBT.Repo.fetch(__MODULE__, schema, id, opts)
 
       @impl VBT.Repo
-      def fetch_by(queryable, clauses, opts \\ []) do
-        import Ecto.Query
-
-        default_opts =
-          if VBT.Repo.schema_module?(queryable),
-            do: [tag: queryable |> Module.split() |> List.last()],
-            else: []
-
-        fetch_one(where(queryable, ^Enum.to_list(clauses)), Keyword.merge(default_opts, opts))
-      end
+      def fetch_by(queryable, clauses, opts \\ []),
+        do: VBT.Repo.fetch_by(__MODULE__, queryable, clauses, opts)
 
       @impl VBT.Repo
-      def fetch_one(queryable, opts \\ []) do
-        {custom_opts, opts} = Keyword.split(opts, ~w/error tag/a)
-
-        error_message =
-          case Keyword.fetch(custom_opts, :error) do
-            {:ok, message} -> message
-            :error -> "#{Keyword.get(custom_opts, :tag, "Record")} not found"
-          end
-
-        # This is a modified implementation of `Repo.one`. We can't use `Repo.one` directly because we
-        # need to distinguish between these two cases:
-        #
-        #     1. The row is not present (we should return an error)
-        #     2. The row is present but the selected value is NULL (we should return `{:ok, nil}`).
-        #
-        # For example, consider the following query:
-        #
-        #     from account in Account, where: account.id == ^id, select: account.phone_number
-        #
-        # If the given account exists, but its phone number is NULL, `fetch` needs to return
-        # `{:ok, nil}`. We can only distinguish between cases 1 and 2 if we invoke `Repo.all` and
-        # branch on the number of returned records.
-        case all(queryable, opts) do
-          [record] -> {:ok, record}
-          [] -> {:error, error_message}
-          other -> raise Ecto.MultipleResultsError, queryable: queryable, count: length(other)
-        end
-      end
+      def fetch_one(queryable, opts \\ []), do: VBT.Repo.fetch_one(__MODULE__, queryable, opts)
 
       @impl VBT.Repo
-      def transact(fun, opts \\ []) do
-        transaction(fn repo ->
-          Function.info(fun, :arity)
-          |> case do
-            {:arity, 0} -> fun.()
-            {:arity, 1} -> fun.(repo)
-          end
-          |> case do
-            {:ok, result} -> result
-            {:error, reason} -> rollback(reason)
-          end
-        end)
-      end
+      def transact(fun, opts \\ []), do: VBT.Repo.transact(__MODULE__, fun, opts)
 
       @impl VBT.Repo
-      def delete_one(query) do
-        with {:ok, result} <-
-               transact(fn ->
-                 case delete_all(query) do
-                   {0, _} -> {:error, :not_found}
-                   {1, result} -> {:ok, result}
-                   _ -> {:error, :multiple_rows}
-                 end
-               end) do
-          case result do
-            nil -> :ok
-            [record] -> {:ok, record}
-          end
-        end
-      end
+      def delete_one(query), do: VBT.Repo.delete_one(__MODULE__, query)
     end
   end
 
   @doc false
-  @spec schema_module?(Ecto.Queryable.t()) :: boolean
-  def schema_module?(queryable),
+  # credo:disable-for-next-line Credo.Check.Readability.Specs
+  def fetch(repo, schema, id, opts) do
+    unless schema_module?(schema), do: raise(ArgumentError, "expected a schema")
+
+    case schema.__schema__(:primary_key) do
+      [primary_key] -> fetch_by(repo, schema, [{primary_key, id}], opts)
+      _other -> raise(ArgumentError, "#{inspect(schema)} must have exactly one primary key")
+    end
+  end
+
+  @doc false
+  # credo:disable-for-next-line Credo.Check.Readability.Specs
+  def fetch_by(repo, queryable, clauses, opts) do
+    default_opts =
+      if schema_module?(queryable),
+        do: [tag: queryable |> Module.split() |> List.last()],
+        else: []
+
+    fetch_one(repo, where(queryable, ^Enum.to_list(clauses)), Keyword.merge(default_opts, opts))
+  end
+
+  @doc false
+  # credo:disable-for-next-line Credo.Check.Readability.Specs
+  def fetch_one(repo, queryable, opts) do
+    {custom_opts, opts} = Keyword.split(opts, ~w/error tag/a)
+
+    error_message =
+      case Keyword.fetch(custom_opts, :error) do
+        {:ok, message} -> message
+        :error -> "#{Keyword.get(custom_opts, :tag, "Record")} not found"
+      end
+
+    # This is a modified implementation of `Repo.one`. We can't use `Repo.one` directly because we
+    # need to distinguish between these two cases:
+    #
+    #     1. The row is not present (we should return an error)
+    #     2. The row is present but the selected value is NULL (we should return `{:ok, nil}`).
+    #
+    # For example, consider the following query:
+    #
+    #     from account in Account, where: account.id == ^id, select: account.phone_number
+    #
+    # If the given account exists, but its phone number is NULL, `fetch` needs to return
+    # `{:ok, nil}`. We can only distinguish between cases 1 and 2 if we invoke `Repo.all` and
+    # branch on the number of returned records.
+    case repo.all(queryable, opts) do
+      [record] -> {:ok, record}
+      [] -> {:error, error_message}
+      other -> raise Ecto.MultipleResultsError, queryable: queryable, count: length(other)
+    end
+  end
+
+  @doc false
+  # credo:disable-for-next-line Credo.Check.Readability.Specs
+  def transact(repo, fun, opts \\ []) do
+    repo.transaction(
+      fn repo ->
+        Function.info(fun, :arity)
+        |> case do
+          {:arity, 0} -> fun.()
+          {:arity, 1} -> fun.(repo)
+        end
+        |> case do
+          {:ok, result} -> result
+          {:error, reason} -> repo.rollback(reason)
+        end
+      end,
+      opts
+    )
+  end
+
+  @doc false
+  # credo:disable-for-next-line Credo.Check.Readability.Specs
+  def delete_one(repo, query) do
+    with {:ok, result} <-
+           transact(repo, fn ->
+             case repo.delete_all(query) do
+               {0, _} -> {:error, :not_found}
+               {1, result} -> {:ok, result}
+               _ -> {:error, :multiple_rows}
+             end
+           end) do
+      case result do
+        nil -> :ok
+        [record] -> {:ok, record}
+      end
+    end
+  end
+
+  defp schema_module?(queryable),
     do: is_atom(queryable) and function_exported?(queryable, :__schema__, 1)
 end
