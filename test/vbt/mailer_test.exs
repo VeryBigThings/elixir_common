5dcdbc655e49088487a6d131a92da83969c4ba13
Saša Jurić <sasa.juric@gmail.com>
2020-05-15 12:30:03 +0200

    Support mail attachments

--- test/vbt/mailer_test.exs
+++ test/vbt/mailer_test.exs
@@ -4,6 +4,14 @@ defmodule VBT.MailerTest do
   alias Ecto.Adapters.SQL.Sandbox
   alias VBT.{Mailer, TestMailer}
 
+  setup_all do
+    # need to force load the test adapter module, because otherwise the attachment test fails
+    # because of lazy module loading in test env and a bamboo internal check
+    Code.ensure_loaded(Bamboo.TestAdapter)
+
+    :ok
+  end
+
   describe "send!" do
     test "sends mail with all fields populated" do
       Mailer.send!(TestMailer, "from@x.y.z", "to@x.y.z", "some subject", "mail body")
@@ -12,6 +20,58 @@ defmodule VBT.MailerTest do
       assert mail.to == [{nil, "to@x.y.z"}]
       assert mail.subject == "some subject"
       assert mail.text_body == "mail body"
+    end
+
+    test "sends an attachment with data" do
+      Mailer.send!(TestMailer, "from@x.y.z", "to@x.y.z", "some subject", "mail body",
+        attachments: [%Bamboo.Attachment{filename: "foo.txt", data: "some content"}]
+      )
+
+      mail = assert_delivered_email()
+      assert [attachment] = mail.attachments
+
+      assert attachment.filename == "foo.txt"
+      assert attachment.data == "some content"
+    end
+
+    test "sends an attachment with file" do
+      tmp_file = Path.join(System.tmp_dir!(), "attachment_#{unique_positive_integer()}.txt")
+      file_data = :crypto.strong_rand_bytes(16)
+      File.write(tmp_file, file_data)
+
+      try do
+        Mailer.send!(TestMailer, "from@x.y.z", "to@x.y.z", "some subject", "mail body",
+          attachments: [%Bamboo.Attachment{path: tmp_file}]
+        )
+
+        mail = assert_delivered_email()
+        assert [attachment] = mail.attachments
+
+        assert attachment.filename == Path.basename(tmp_file)
+        assert attachment.data == file_data
+      after
+        File.rm_rf(tmp_file)
+      end
+    end
+
+    test "doesn't overwrite a filename if provided" do
+      tmp_file = Path.join(System.tmp_dir!(), "attachment_#{unique_positive_integer()}.txt")
+      file_data = :crypto.strong_rand_bytes(16)
+      File.write(tmp_file, file_data)
+
+      try do
+        Mailer.send!(TestMailer, "from@x.y.z", "to@x.y.z", "some subject", "mail body",
+          attachments: [%Bamboo.Attachment{path: tmp_file, filename: "bar.txt"}]
+        )
+
+        mail = assert_delivered_email()
+        assert [attachment] = mail.attachments
+
+        assert attachment.filename == "bar.txt"
+        assert attachment.data == file_data
+      after
+        File.rm_rf(tmp_file)
+      end
     end
 
     test "sets text and html body" do
@@ -49,6 +109,19 @@ defmodule VBT.MailerTest do
       assert mail.to == [{nil, "to@x.y.z"}]
       assert mail.subject == "some subject"
       assert mail.text_body == "mail body"
+    end
+
+    test "sends attachment if provided" do
+      Mailer.enqueue(TestMailer, "from@x.y.z", "to@x.y.z", "some subject", "mail body",
+        attachments: [%Bamboo.Attachment{filename: "foo.txt", data: "some content"}]
+      )
+
+      TestMailer.drain_queue()
+
+      mail = assert_delivered_email()
+      assert [attachment] = mail.attachments
+      assert attachment.filename == "foo.txt"
+      assert attachment.data == "some content"
     end
   end
 end
