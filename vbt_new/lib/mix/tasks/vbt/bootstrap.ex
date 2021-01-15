3eca71ae552cf9cfc5ea96720c10609b9b5003fe
ademirovic <azra.demirovic@verybigthings.com>
2021-01-15 16:25:50 +0100

    Remove dialyzer and credo from preffered cli
    The default env for dialyzer and credo is dev, so this is redundant.

--- vbt_new/lib/mix/tasks/vbt/bootstrap.ex
+++ vbt_new/lib/mix/tasks/vbt/bootstrap.ex
@@ -199,7 +199,7 @@ defmodule Mix.Tasks.Vbt.Bootstrap do
     |> MixFile.append_config(:project, "preferred_cli_env: preferred_cli_env()")
     |> SourceFile.add_to_module("""
     defp preferred_cli_env,
-      do: [credo: :dev, dialyzer: :dev, release: :prod, operator_template: :prod]
+      do: [release: :prod, operator_template: :prod]
 
     """)
   end
--- vbt_new/test_projects/expected_state/mix.exs
+++ vbt_new/test_projects/expected_state/mix.exs
@@ -75,7 +75,7 @@ defmodule SkafolderTester.MixProject do
   end
 
   defp preferred_cli_env,
-    do: [credo: :dev, dialyzer: :dev, release: :prod, operator_template: :prod]
+    do: [release: :prod, operator_template: :prod]
 
   defp dialyzer do
     [
