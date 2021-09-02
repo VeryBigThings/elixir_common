6739b1e7bde6f36fe3d69e33452486a572f15210
Kenneth Kostresevic <kenneth.kostresevic@verybigthings.com>
2021-09-02 10:05:23 +0200

    WIP

--- vbt_new/test_projects/expected_state/mix.exs
+++ vbt_new/test_projects/expected_state/mix.exs
@@ -38,20 +38,20 @@ defmodule SkafolderTester.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:boundary, "~> 0.6"},
-      {:ecto_sql, "~> 3.4"},
-      {:gettext, "~> 0.11"},
-      {:jason, "~> 1.0"},
-      {:mox, "~> 0.5", only: :test},
-      {:phoenix, "~> 1.5.10"},
-      {:phoenix_ecto, "~> 4.1"},
-      {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_dashboard, "~> 0.4"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:plug_cowboy, "~> 2.0"},
-      {:postgrex, ">= 0.0.0"},
-      {:telemetry_metrics, "~> 0.4"},
-      {:telemetry_poller, "~> 0.4"},
+      {:boundary, "~> 0.8.0"},
+      {:ecto_sql, "~> 3.7"},
+      {:gettext, "~> 0.18.2"},
+      {:jason, "~> 1.2"},
+      {:mox, "~> 1.0", only: :test},
+      {:phoenix, "~> 1.5.12"},
+      {:phoenix_ecto, "~> 4.4"},
+      {:phoenix_html, "~> 2.13"},
+      {:phoenix_live_dashboard, "~> 0.5.0"},
+      {:phoenix_live_reload, "~> 1.3", only: :dev},
+      {:plug_cowboy, "~> 2.5"},
+      {:postgrex, "~> 0.15.10"},
+      {:telemetry_metrics, "~> 0.6.1"},
+      {:telemetry_poller, "~> 1.0"},
       {:vbt, path: "../../../.."}
     ]
   end
